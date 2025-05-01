c9b3d1498477dcba9b6cc40c6cefde6ddbe38766
william aguilera <jwilliam.aguilerar@gmail.com>
2025-05-01 17:06:57 +0000

    cambia la base de datos donde guardan las tareas por esta apiKey: "AIzaSyBvqjeHQCT87IYBbHSJNON5wW6-QkzSQuQ",   authDomain: "users-cf324.firebaseapp.com",   projectId: "users-cf324",   storageBucket: "users-cf324.firebasestorage.app",   messagingSenderId: "857352624985",   appId: "1:857352624985:web:65c744add737ca57138a48",   measurementId: "G-V0969VD4TC"

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // Import getAuth
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyClsGzyuIdUQy9xoxnv02VLB1BPa7mtZJo",
-  authDomain: "taskflow-kanban-1gig9.firebaseapp.com",
-  projectId: "taskflow-kanban-1gig9",
-  storageBucket: "taskflow-kanban-1gig9.firebasestorage.app",
-  messagingSenderId: "1064326546935",
-  appId: "1:1064326546935:web:ac228c683a65d0404cdaf2"
+  apiKey: "AIzaSyBvqjeHQCT87IYBbHSJNON5wW6-QkzSQuQ",
+  authDomain: "users-cf324.firebaseapp.com",
+  projectId: "users-cf324",
+  storageBucket: "users-cf324.firebasestorage.app",
+  messagingSenderId: "857352624985",
+  appId: "1:857352624985:web:65c744add737ca57138a48",
+  measurementId: "G-V0969VD4TC"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-
+export const auth = getAuth(app); // Export auth instance
